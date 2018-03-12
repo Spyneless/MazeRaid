0553ad396c679f238e256101cdd233b7595da585
Timon van Zanten <timonvanzanten@gmail.com>
2018-03-12 21:46:02 +0100

    init commit 2

---
+++ Assets/Scripts/MazeManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour {
+
+    public int width;
+    public int height;
+    public GameObject tile;
+
+    private TileScript[][] grid;// = new TileScript[width][];
+
+	// Use this for initialization
+	void Start () {
+        grid = new TileScript[width][];
+        for (int x = 0; x < width; x++)
+        {
+            grid[x] = new TileScript[height];
+            for (int z = 0; z < height; z++)
+            {
+                GameObject t = GameObject.Instantiate(tile);
+                t.transform.parent = transform;
+                t.name = "tile (" + x + "-" + z + ")";
+                t.transform.position = new Vector3(x * 1.1f, 0, z * 1.1f);
+
+                TileScript ts = t.GetComponent<TileScript>();
+                grid[x][z] = ts;
+                ts.coord = new Vector2(x, z);
+                ts.MazeDimensions = new Vector2(width, height);
+                ts.SetConnections();
+            }
+        }
+        for (int x = 0; x < width; x++)
+            for (int z = 0; z < height; z++)
+                grid[x][z].SetTileScriptLinks(grid);
+
+        GenerateMazePattern();
+
+        for (int x = 0; x < width; x++)
+            for (int z = 0; z < height; z++)
+                grid[x][z].SetWalls();
+}
+
+    private void GenerateMazePattern()
+    {
+        SortedList<int, TileScript> mazePathsTo = new SortedList<int, TileScript>();
+        SortedList<int, TileScript> mazePathsFrom = new SortedList<int, TileScript>();
+
+        TileScript source = grid[0][0];
+
+        //add north and east connection
+        mazePathsFrom.Add(source.connectionValues[0], source);
+        mazePathsFrom.Add(source.connectionValues[1], source);
+        mazePathsTo.Add(source.connectionValues[0], source.connections[0]);
+        mazePathsTo.Add(source.connectionValues[1], source.connections[1]);
+
+        while(mazePathsTo.Count != 0)
+        {
+            
+            TileScript from = mazePathsFrom.Values[0];
+            TileScript to = mazePathsTo.Values[0];
+            mazePathsFrom.RemoveAt(0);
+            mazePathsTo.RemoveAt(0);
+
+            to.Discovered = true;
+
+            for(int i = 0; i < 4; i++)
+            {
+                if (to.connectionValues[i] == -1)
+                    break;
+                TileScript nb = to.connections[i];
+                if (!nb.Discovered )
+                {
+                    //add
+                    //WHYis it double adding
+                    int newpathvalue = to.connectionValues[i];
+                    if (!mazePathsTo.ContainsKey(newpathvalue))
+                    {
+                        mazePathsTo.Add(newpathvalue, nb);
+                        mazePathsFrom.Add(newpathvalue, to);
+                    }
+                }
+                else if(nb.connections[i] == from)
+                {
+                    //don do anything
+                }
+                else
+                {
+                    nb.BlockPath(to);
+                    //blockpath
+                    to.BlockPath(nb);
+                }
+            }
+        }
+
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    public int width;
+    public Vector2 coord;
+    public Vector2 MazeDimensions;
+    public GameObject wall;
+    public int[] connectionValues = new int[4];
+    public TileScript[] connections = new TileScript[4];
+    public bool Discovered {get; set;}
+
+    public Dictionary<TileScript, int> whichNeighbour;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetConnections()
+    {
+        Discovered = false;
+        for(int i = 0; i < 4; i++)
+        {
+            connectionValues[i] = Random.Range(0, 1000000);
+        }
+
+        if (coord.y == MazeDimensions.y - 1)
+            connectionValues[0] = -1;
+        if (coord.x == MazeDimensions.x -1)
+            connectionValues[1] = -1;
+        if (coord.y == 0)
+            connectionValues[2] = -1;
+        if (coord.x == 0)
+            connectionValues[3] = -1;
+    }
+
+    public void SetWalls()
+    {
+        if (connectionValues[0] == -1)
+            AddWall(0, 0.5f, 90f);
+        if (connectionValues[1] == -1)
+            AddWall(0.5f, 0, 0);
+        if (connectionValues[2] == -1)
+            AddWall(0, -0.5f, 90f);
+        if (connectionValues[3] == -1)
+            AddWall(-0.5f, 0, 0);
+    }
+
+    public void AddWall(float x, float z, float rot)
+    {
+        GameObject w = GameObject.Instantiate(wall);
+        w.transform.parent = transform;
+        w.transform.position = new Vector3(transform.position.x + x, 0.5f, transform.position.z + z);
+        w.transform.Rotate(new Vector3(0, 1, 0), rot);
+    }
+
+    public void SetTileScriptLinks(TileScript[][] grid)
+    {
+        whichNeighbour = new Dictionary<TileScript, int>();
+
+        //north
+        if (coord.y != MazeDimensions.y - 1)
+        {
+            connections[0] = grid[(int)coord.x][(int)coord.y + 1];
+            whichNeighbour.Add(connections[0], 0);
+        }
+        else
+            connections[0] = null;
+
+        //east
+        if (coord.x != MazeDimensions.x - 1)
+        {
+            connections[1] = grid[(int)coord.x + 1][(int)coord.y];
+            whichNeighbour.Add(connections[1], 1);
+        }
+        else
+            connections[1] = null;
+
+        //south
+        if (coord.y != 0)
+        { 
+            connections[2] = grid[(int)coord.x][(int)coord.y - 1];
+            whichNeighbour.Add(connections[2], 2);
+        }
+        else
+            connections[2] = null;
+
+        //west
+        if (coord.x != 0)
+        {
+            connections[3] = grid[(int)coord.x - 1][(int)coord.y];
+            whichNeighbour.Add(connections[3], 3);
+        }
+        else
+            connections[3] = null;
+
+    }
+
+    public void BlockPath(TileScript nb)
+    {
+        connectionValues[whichNeighbour[nb]] = -1;
+    }
+}
