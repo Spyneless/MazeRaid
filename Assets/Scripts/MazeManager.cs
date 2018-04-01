f41d7d0ad18b40c2e8ea5fd54693e4a3cf0deaa7
Timon van Zanten <timonvanzanten@gmail.com>
2018-04-01 19:02:35 +0200

    basic plan for segments laid out

--- Assets/Scripts/MazeManager.cs
+++ Assets/Scripts/MazeManager.cs
@@ -5,146 +5,34 @@ using UnityEngine;
 
 public class MazeManager : MonoBehaviour {
 
-    public int width;
-    public int height;
+    public int Width;
+    public int Height;
+    public int SegmentHeight;
+    public int SegmentWidth;
+
     public GameObject tile;
 
-    private TileScript[][] grid;// = new TileScript[width][];
+    private SegmentManager[][] grid;// = new TileScript[width][];
 
 	// Use this for initialization
 	void Start () {
-        int id = 0;
-        grid = new TileScript[width][];
-        for (int x = 0; x < width; x++)
-        {
-            grid[x] = new TileScript[height];
-            for (int z = 0; z < height; z++)
-            {
-                GameObject t = GameObject.Instantiate(tile);
-                t.transform.parent = transform;
-                t.name = "tile (" + x + "-" + z + ")";
-                t.transform.position = new Vector3(x * 1.1f, 0, z * 1.1f);
+        //for x*y 
+        //generate segment
+        //set parameters
 
-                TileScript ts = t.GetComponent<TileScript>();
-                grid[x][z] = ts;
-                ts.coord = new Vector2(x, z);
-                ts.MazeDimensions = new Vector2(width, height);
-                ts.SetConnections();
+        //for x*y
+        //set links for segments
 
-                ts.Origin = -1;
-                ts.ID = id;
-                id++;
-            }
-        }
-        for (int x = 0; x < width; x++)
-            for (int z = 0; z < height; z++)
-                grid[x][z].SetTileScriptLinks(grid);
+        //generate mazepattern between segments
+            //--number predecessr and distance to origin
+            //--keep track of connections
+        //(optional) add few more passages
 
-        GenerateMazePattern();
+        //for x*y
+        //call setup of the segments
 
-        for (int x = 0; x < width; x++)
-            for (int z = 0; z < height; z++)
-            {
-                for (int i = 0; i < 4; i++)
-                    Debug.Log(grid[x][z].name + " connection " + i + " = " + grid[x][z].connectionValues[i]);
-            }
-
-        for (int x = 0; x < width; x++)
-            for (int z = 0; z < height; z++)
-                grid[x][z].SetWalls();
-}
-
-    private void GenerateMazePattern()
-    {
-        SortedDictionary<int, TileScript> mazePathsTo = new SortedDictionary<int, TileScript>();
-        SortedDictionary<int, TileScript> mazePathsFrom = new SortedDictionary<int, TileScript>();
-
-        TileScript source = grid[0][0];
-        source.Discovered = true;
-        source.Origin = source.ID;
-
-        //add north and east connection
-        mazePathsFrom.Add(source.connectionValues[0], source);
-        mazePathsFrom.Add(source.connectionValues[1], source);
-        mazePathsTo.Add(source.connectionValues[0], source.connections[0]);
-        mazePathsTo.Add(source.connectionValues[1], source.connections[1]);
-
-        int it = 0;
-
-        while(mazePathsTo.Count != 0)
-        {
-            foreach(KeyValuePair<int, TileScript> kvp in mazePathsFrom)
-            {
-                Debug.Log("iteration " + it + "   key-" + kvp.Key + " " + kvp.Value.name + " -> " + mazePathsTo[kvp.Key] );
-                it++;
-            }
-            Debug.Log("---");
-
-            TileScript from = null;
-            TileScript to = null;
-            int index = 0;
-
-            foreach (KeyValuePair<int, TileScript> kvp in mazePathsFrom)
-            {
-                from = kvp.Value;
-                index = kvp.Key;
-                break;
-            }
-            foreach (KeyValuePair<int, TileScript> kvp in mazePathsTo)
-            {
-                to = kvp.Value;
-                break;
-            }
-
-            Debug.Log("OOOOO key-" + index + " " + from.name.ToString() + " -> " + to.name.ToString());
-            
-            mazePathsFrom.Remove(index);
-            mazePathsTo.Remove(index);
-            if (to.Discovered)
-            { }
-            else
-            {
-                to.Discovered = true;
-                to.Origin = from.ID;
-
-                for (int i = 0; i < 4; i++)
-                {
-                    //"neighbour" would go out of bounds or is already closed off
-                    if (to.connectionValues[i] == -1)
-                    { }
-                    else
-                    {
-                        TileScript nb = to.connections[i];
-
-                        //if not discovered, add the connection to the list
-                        if (!nb.Discovered)
-                        {
-                            //add
-                            int newpathvalue = to.connectionValues[i];
-                            if (!mazePathsTo.ContainsKey(newpathvalue))
-                            {
-                                mazePathsTo.Add(newpathvalue, nb);
-                                mazePathsFrom.Add(newpathvalue, to);
-                            }
-                            Debug.Log("discovered-" + to + "    new node-" + nb.name);
-                        }
-                        //else if discovered AND it is not your predecessor, block the path
-                        //else if (nb.connections[i] != null)
-                        //{
-                        else if (nb.ID != to.Origin)
-                        {
-                            nb.BlockPath(to);
-                            //blockpath
-                            to.BlockPath(nb);
-                        }
-                        //}
-                    }
-                }
-                Debug.Log("//---//");
-            }
-        }
-
-
+        //for x*y
+        //break through boundaries between segments
     }
 
 	// Update is called once per frame
