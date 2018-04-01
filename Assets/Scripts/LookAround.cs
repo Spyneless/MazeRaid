01ffe1a53e1be22377c1a32a1037312e14599e9a
Timon van Zanten <timonvanzanten@gmail.com>
2018-04-01 18:50:27 +0200

    generating maze + walking!

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour {
+
+    public float speedH = 2;
+    public float speedY = 2;
+
+    private float yaw;
+    private float pitch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedY * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float baseSpeed = 3.0f;
+    private float speed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        speed = baseSpeed;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        speed = baseSpeed;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        Vector3 dirFront = z * Camera.main.transform.forward;  // front
+        Vector3 dirRight = x * Camera.main.transform.right;  // right;
+
+        Vector3 dir = dirFront + dirRight;
+        dir = dir.normalized;
+
+        rb.AddForce(dir * speed, ForceMode.VelocityChange);
+
+        //transform.Translate(dirFront.x, 0, dirFront.z);
+        //transform.Translate(dirRight.x, 0, dirRight.z);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Wall")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            //rigidbody.velocity = Vector3.zero;
+            speed = 0;
+        }
+    }
+}
